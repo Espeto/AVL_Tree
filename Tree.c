0adc7888f37f53daad172b8a4d683b1664c46d31
Gilberto Kreisler <gkreisler5@hotmail.com>
2015-06-09 22:56:27 -0300

    Implemented Function double rotation to the left and to the right. Must fix functions simple rotation to the left and to the right. Incomplete function insert.

--- Tree.c
+++ Tree.c
@@ -1,5 +1,5 @@
 /* 
- * File:   main.c
+ * File:   Tree.c
  * Author: gilberto
  *
  * Created on 7 de Junho de 2015, 05:16
@@ -33,10 +33,10 @@ node *start(void){
     node *head;
     
     head = malloc(sizeof(node));
-    head->balance = 0;
     head->left = NULL;
     head->right = NULL;
     
+    return head;
 }
 
 //Incial Menu
@@ -56,10 +56,14 @@ int menu(void){
 }
 
 //Insert function
-void insert(node *root){
+void insert(node *root, int value){
     node *father, *aux;
+    int find = 0; //Loop control
     
     father = root;
+    
+    while((!find) &&  )
+    
 }
 
 //Simple left rotation
@@ -86,10 +90,36 @@ void simpleRight(node *root){
 
 //Double left rotation
 void doubleLeft(node *root){
+    node *f_aux, *r_aux, *sub_l;
+    //Attribution
+    f_aux = root;
+    r_aux = root->right;
+    sub_l = r_aux->left;
     
+    //Firt rotation on sons
+    r_aux->left = sub_l->right;
+    sub_l->right= r_aux;
+    f_aux->right = sub_l;
+    
+    //Final rotation on root
+    f_aux->right = sub_l->left;
+    sub_l->left = f_aux;
 }
 
 //Double right rotation
 void doubleRight(node *root){
+    node *f_aux, *l_aux, *sub_r;
+    //Attribution
+    f_aux = root;
+    l_aux = root->left;
+    sub_r = l_aux->right;
     
+    //First rotation on sons
+    l_aux->right = sub_r->left;
+    sub_r->left = l_aux;
+    f_aux->left = sub_r;
+    
+    //Final rotation on root
+    f_aux->left = sub_r->right;
+    sub_r->right = f_aux;
 }
